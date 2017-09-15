6f2695d14564490351a63d81e33a11c123db1e4b
hazrmard <ibrahim78786@gmail.com>
2017-09-15 16:41:25 -0500

    isolated source/draft and deploy beanch

--- blog_src/themes/hugolb/static/js/resume.js
+++
@@ -1,8 +0,0 @@
-console.log(RESUME_JSON_PATH);
-var Resume = angular.module("Resume", []);
-Resume.controller("resumeController", function($scope, $http) {
-    $http.get(RESUME_JSON_PATH)
-        .success(function(response){
-            $scope.resumeData = response;
-        });
-});
